8cf59afcd307bda8395b134886a71d509aacd6d8
yurinvv <yurinvv@mail.ru>
2022-09-18 13:56:06 +0300

    Учебный проект. Начало

---
+++ yurinvvspring/src/test/java/com/example/yurinvvspring/YurinvvspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yurinvvspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YurinvvspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
